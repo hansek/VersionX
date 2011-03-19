0e2f6d9108f13482919a43c05a84b41b2b9b0189
Mark-H <business@markhamstra.nl>
2011-03-19 16:10:23 +0100

    Fix critical issue with German lexicon file. (smooth-graphics)

--- core/components/versionx/lexicon/de/default.inc.php
+++ core/components/versionx/lexicon/de/default.inc.php
@@ -26,17 +26,15 @@
  * @date 2011-01-11
  * 
 */
- 
-$vxp = 'versionx.'; // I'm lazy, I know.
- 
-// Global 
+
+$vxp = 'versionx.';
+
 $_lang[$vxp.'versionx'] = 'VersionX';
- 
-// Manager: Component menu
+
 $_lang[$vxp.'menuDesc'] = 'Komponente für Inhalt-Versionierung';
- 
-// CMP: Overview
+
 $_lang[$vxp.'overviewTitle'] = 'Übersicht';
+
 $_lang[$vxp.'overviewDescription'] = 'VersionX überwacht sämtliche Änderungen in Ressourcen, Chunks und Snippets. Es bietet die Möglichkeit ältere Versionen per One-Click wiederherzustellen und Versionen zu vergleichen.<br>Für detaillierte Informationen wechseln Sie in den Reiter der gewünschten Ressourcengruppe.';
 $_lang[$vxp.'devTitle'] = 'Entwicklung';
 $_lang[$vxp.'devDescription'] = 'VersionX wurde von <a href="http://www.markhamstra.nl" title="Mark Hamstra">Mark Hamstra</a> als einfaches Versionierungs-Addon für <a href="http://www.modx.com" title="MODx: Content Management Framework">MODx CMF</a> entwickelt.<br>Bitte melden Sie sämtliche Bugs, unerwartetes Verhalten oder Feature-Requests im <a href="https://github.com/Mark-H/VersionX" title="VersionX on Github">Github Repository</a>.';
@@ -44,14 +42,11 @@ $_lang[$vxp.'devDescription'] = 'VersionX wurde von <a href="http://www.markhams
 // CMP: Resources
 $_lang[$vxp.'resourcesTitle'] = 'Ressourcen';
 $_lang[$vxp.'resourcesDescription'] = 'VersionX erfasst sämtliche Änderungen an Ihren Inhalten.';
- 
-// Resource detail window
 $_lang[$vxp.'detailwindow.title'] = 'Ressourcen Änderung';
 $_lang[$vxp.'detailwindow.basictab'] = 'Dokument Felder';
 $_lang[$vxp.'detailwindow.settingstab'] = 'Seiteneinstellungen';
 $_lang[$vxp.'detailwindow.contenttab'] = 'Inhalt';
- 
-// Resource compare window
+
 $_lang[$vxp.'comparewindow.fieldstab'] = 'Felder &amp; Seiteneinstellungen'; 
 $_lang[$vxp.'comparewindow.contenttab'] = 'Inhalt';
 $_lang[$vxp.'comparewindow.fields.field'] = 'Feld';
@@ -60,16 +55,13 @@ $_lang[$vxp.'comparewindow.fields.new'] = 'neu';
 $_lang[$vxp.'comparewindow.fields.change'] = 'geändert';
 $_lang[$vxp.'added'] = 'hinzugefügt';
 $_lang[$vxp.'removed'] = 'entfernt';
- 
-// CMP: Chunks
+
 $_lang[$vxp.'chunksTitle'] = 'Chunks';
-$_lang[$vxp.'chunksDescription']  = 'Objekte, die global verwendet werden können sollten überprüfbar sein. VersionX ermöglicht das und kann die Unterschiede zwischen den verschiedenen Änderungen anzeigen.';
- 
-// CMP: Snippets
+//$_lang[$vxp.'chunksDescription'] = 'Objekte, die global verwendet werden können sollten überprüfbar sein. VersionX ermöglicht das und kann die Unterschiede zwischen den verschiedenen Änderungen anzeigen.';
+
 $_lang[$vxp.'snippetsTitle'] = 'Snippets';
 $_lang[$vxp.'snippetsDescription'] = 'Änderungen an Snippets können es schnell unbrauchbar machen. Stellen Sie es mit VersionX wieder her';
- 
-// CMP: Grid headings
+
 $_lang[$vxp.'grid.revNum'] = '#';
 $_lang[$vxp.'grid.docID'] = 'Ressource ID';
 $_lang[$vxp.'grid.mode'] = 'Modus';
@@ -80,16 +72,14 @@ $_lang[$vxp.'grid.fromRev'] = 'vorherige Version';
 $_lang[$vxp.'grid.class'] = 'Klasse';
 $_lang[$vxp.'grid.time'] = 'Datum';
 $_lang[$vxp.'grid.editor'] = 'Editor';
- 
-// CMP: Grid context menu
+
 $_lang[$vxp.'grid.details'] = 'Details ansehen';
 $_lang[$vxp.'grid.compare'] = 'Vergleichen';
 $_lang[$vxp.'grid.restore'] = 'Wiederherstellen';
 $_lang[$vxp.'grid.restoreTitle'] = 'Version wiederherstellen';
 $_lang[$vxp.'grid.restoreMsg'] = 'Sind Sie sicher, dass Sie diese Ressource mit der gewählten (evtl. veralteten) Version ersetzen möchten?';
 $_lang[$vxp.'grid.restore.done'] = 'Die ausgewählte Version wurde wiederhergestellt.';
- 
-// CMP: Error messages (New 14/1/2011)
+
 $_lang[$vxp.'error.idnotfound'] = 'Konnte Versions-ID nicht finden. Die Versionstabelle könnte beschädigt sein.';
 $_lang[$vxp.'error.requestfailed'] = 'Laden der benötigten Daten fehlgeschlagen! Bitte prüfen Sie Ihre Verbindung.';
 $_lang[$vxp.'error.packagenotfound'] = 'fataler Fehler: VersionX konnte nicht registriert werden.';
@@ -97,8 +87,7 @@ $_lang[$vxp.'error.revsnotfound'] = 'fataler Fehler: keine Versionsnummer angege
 $_lang[$vxp.'error.revobjectnotfound'] = 'fataler Fehler: konnte Version des Objektes nicht finden.';
 $_lang[$vxp.'error.revsdontmatch'] = 'fataler Fehler: die beiden angegebenen Versionen gehören nicht zur gleichen Ressource.';
 $_lang[$vxp.'error.nochangesfound'] = 'Keine Änderungen gefunden.';
- 
-// (Hopefully) temporary error for xpdorevisioncontrol error for php > 5.3.0
+
 $_lang[$vxp.'error.splitdepreciated'] = 'VersionX ist momentan nicht kompatibel mit PHP 5.3.x :(';
  
 ?>
